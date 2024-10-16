c0870f2075f7b234bbe980416011e3bef2487ab8
BW <17097491+bw-01@users.noreply.github.com>
2024-10-17 11:31:50 +1300

    Add: loading data from user input location

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 
-let loc = "wellington";
+let defaultLocation = "wellington";
 const weatherAll = [];
 
 // Stores daily weather data
@@ -17,7 +17,7 @@ class Weather {
   }
 }
 
-// Load the weather data from visualcrossing.com 
+// Load the weather data from visualcrossing.com
 async function fetchWeatherData(location) {
   const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next6days?unitGroup=us&elements=datetime%2Cname%2Ctempmax%2Ctempmin%2Ctemp%2Cfeelslike%2Cprecipprob%2Cconditions%2Cdescription%2Cicon&include=days%2Cfcst&key=XR9HGK7NHMV8HBK9BCTNUXUR8&contentType=json`;
   const response = await fetch(url, { mode: "cors" });
@@ -32,24 +32,33 @@ async function fetchWeatherData(location) {
 }
 
 // Process any loaded data into Weather class objects
-function processData(data) {
-  data.forEach((element) => {
-    const weather = new Weather(element);
-    weatherAll.push(weather);
-  });
+async function processData(location) {
+  try {
+    const data = await fetchWeatherData(location);
+    if (data && data.days) {
+      weatherAll.length = 0;
+
+      data.days.forEach((element) => {
+        const weather = new Weather(element);
+        weatherAll.push(weather);
+      });
+
+      console.log("Data processed for:", location);
+      console.log(weatherAll);
+    } else {
+      console.log("No valid data available");
+    }
+  } catch (error) {
+    console.error("Error handling weather data:", error);
+  }
 }
 
-// Main
-fetchWeatherData(loc)
-  .then((data) => {
-    if (data && data.days) {
-      processData(data.days);
-    } else {
-      console.log("No valid data available to process.");
-    }
-  })
-  .catch((error) => {
-    console.error("Error fetching weather data:", error);
-  });
+processData(defaultLocation);
 
-console.log(weatherAll);
+// Main
+const locationForm = document.querySelector(".location-form");
+locationForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const location = document.getElementById("location-input").value;
+  processData(location);
+});
