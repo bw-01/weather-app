0764de408244a9eda07c2ce41b1619985ef2fa8c
BW <17097491+bw-01@users.noreply.github.com>
2024-10-17 11:03:03 +1300

    Added error handling for loading data

--- src/index.js
+++ src/index.js
@@ -3,37 +3,53 @@ import "./styles.css";
 let loc = "wellington";
 const weatherAll = [];
 
+// Stores daily weather data
 class Weather {
-    constructor(data) {
-        this.date = data.datetime;
-        this.currentTemp = data.temp;
-        this.maxTemp = data.tempmax;
-        this.minTemp = data.tempmin;
-        this.feelsTemp = data.feelslike;
-        this.precipitation = data.precipprob;
-        this.conditions = data.icon;
-        this.description = data.description;
-    }
+  constructor(data) {
+    this.date = data.datetime;
+    this.currentTemp = data.temp;
+    this.maxTemp = data.tempmax;
+    this.minTemp = data.tempmin;
+    this.feelsTemp = data.feelslike;
+    this.precipitation = data.precipprob;
+    this.conditions = data.icon;
+    this.description = data.description;
+  }
 }
 
-async function getWeatherData(location) {
-  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next7days?unitGroup=us&elements=datetime%2Cname%2Ctempmax%2Ctempmin%2Ctemp%2Cfeelslike%2Cprecipprob%2Cconditions%2Cdescription%2Cicon&include=days%2Cfcst&key=XR9HGK7NHMV8HBK9BCTNUXUR8&contentType=json`;
+// Load the weather data from visualcrossing.com 
+async function fetchWeatherData(location) {
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next6days?unitGroup=us&elements=datetime%2Cname%2Ctempmax%2Ctempmin%2Ctemp%2Cfeelslike%2Cprecipprob%2Cconditions%2Cdescription%2Cicon&include=days%2Cfcst&key=XR9HGK7NHMV8HBK9BCTNUXUR8&contentType=json`;
   const response = await fetch(url, { mode: "cors" });
+
+  if (!response.ok) {
+    const errorText = await response.text();
+    throw new Error(errorText);
+  }
+
   const weatherData = await response.json();
-  
-  //console.log(weatherData);
   return weatherData;
 }
 
+// Process any loaded data into Weather class objects
 function processData(data) {
-    data.forEach(element => {
-        const weather = new Weather(element);
-        weatherAll.push(weather);
-    });
+  data.forEach((element) => {
+    const weather = new Weather(element);
+    weatherAll.push(weather);
+  });
 }
 
-getWeatherData(loc).then((data) => {
-  processData(data.days);
-});
+// Main
+fetchWeatherData(loc)
+  .then((data) => {
+    if (data && data.days) {
+      processData(data.days);
+    } else {
+      console.log("No valid data available to process.");
+    }
+  })
+  .catch((error) => {
+    console.error("Error fetching weather data:", error);
+  });
 
 console.log(weatherAll);
